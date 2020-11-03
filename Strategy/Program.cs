ac69f201c67b2518f887ab6081c394bc9625244c
kokojae <54296776+kokojae@users.noreply.github.com>
2020-11-03 11:42:15 +0900

    스트래티지 패턴
    런타임에서 처리방법을 비교적 쉽게 바꿀 수 있는 스트래티지 패턴이다.
    원본 java 코드를 cs로 바꾸면서
    java의 super는 cs에서 base가 같은 역할을 하고있으며
    생성자 호출법도 약간 다르단것을 배울 수 있었다.

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// 참고 : https://gmlwjd9405.github.io/2018/07/06/strategy-pattern.html
+
+using System;
 
 namespace Strategy
 {
@@ -6,7 +8,90 @@ namespace Strategy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Robot taekwonV = new TaekwonV("TaekwonV");
+            Robot atom = new Atom("Atom");
+
+            taekwonV.SetMovingStrategy(new WalkingStrategy());
+            taekwonV.SetAttackStrategy(new MissileStrategy());
+            atom.SetMovingStrategy(new FlyingStrategy());
+            atom.SetAttackStrategy(new PunchStrategy());
+
+            Console.WriteLine("My name is " + taekwonV.GetName());
+            taekwonV.Move();
+            taekwonV.Attack();
+
+            Console.WriteLine();
+            Console.WriteLine("My name is " + atom.GetName());
+            atom.Move();
+            atom.Attack();
+        }
+    }
+
+    public abstract class Robot
+    {
+        private string name;
+        private IAttackStrategy attackStrategy;
+        private IMovingStrategy movingStrategy;
+
+        public Robot(string name) { this.name = name; }
+        public string GetName() { return name; }
+        public void Attack() { attackStrategy.Attack(); }
+        public void Move() { movingStrategy.Move(); }
+
+        public void SetAttackStrategy(IAttackStrategy attackStrategy)
+        {
+            this.attackStrategy = attackStrategy;
+        }
+        public void SetMovingStrategy(IMovingStrategy movingStrategy)
+        {
+            this.movingStrategy = movingStrategy;
+        }
+    }
+
+    public class TaekwonV : Robot
+    {
+        public TaekwonV(string name) : base(name) { }
+    }
+    public class Atom : Robot
+    {
+        public Atom(string name) : base(name) { }
+    }
+
+    public interface IAttackStrategy
+    {
+        public void Attack();
+    }
+    public class MissileStrategy : IAttackStrategy
+    {
+        public void Attack()
+        {
+            Console.WriteLine("I have Missile.");
+        }
+    }
+    public class PunchStrategy : IAttackStrategy
+    {
+        public void Attack()
+        {
+            Console.WriteLine("I have strong punch.");
+        }
+    }
+
+    public interface IMovingStrategy
+    {
+        public void Move();
+    }
+    public class FlyingStrategy : IMovingStrategy
+    {
+        public void Move()
+        {
+            Console.WriteLine("I can fly.");
+        }
+    }
+    public class WalkingStrategy : IMovingStrategy
+    {
+        public void Move()
+        {
+            Console.WriteLine("I can only walk.");
         }
     }
 }
