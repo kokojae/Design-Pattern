776674a9d689ab423f05bd50e44bbd43bd49484e
kokojae <54296776+kokojae@users.noreply.github.com>
2020-11-20 16:42:14 +0900

    Command 패턴
    이벤트가 발생했을 때 실행될 기능이 다양하면서도 변경이 필요한 경우에 이벤트를 발생시키는 클래스를 변경하지 않고 재사용하고자 할 때 유용한 패턴이다.
    
    스트래티지와의 차이점은
    
    스트래티지 패턴은
    객체와 행동이 분리되어 있고, 행동을 어디에나 이식할 수 있는걸 강점으로 삼는다.
    
    커맨드 패턴은
    인보커와 커맨드가 분리되어있고, 인보커는 커맨드의 특정 메소드만 불러주면 거기 안에 들어있는 리시버가 지정된 작업을 수행한다.
    
    라고 한다.

---
+++ Command/Program.cs
@@ -0,0 +1,73 @@
+﻿// 참고 : https://gmlwjd9405.github.io/2018/07/07/command-pattern.html
+
+using System;
+
+namespace Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lamp lamp = new Lamp();
+            ICommand lampOnCommand = new LampOnCommand(lamp);
+            Alarm alarm = new Alarm();
+            ICommand alarmStartCommand = new AlarmStartCommand(alarm);
+
+            Button button1 = new Button(lampOnCommand); // 램프 켜는 Command 설정
+            button1.Pressed(); // 램프 켜는 기능 수행
+
+            Button button2 = new Button(alarmStartCommand); // 알람 울리는 Command 설정
+            button2.Pressed(); // 알람 울리는 기능 수행
+            button2.SetCommand(lampOnCommand); // 다시 램프 켜는 Command로 설정
+            button2.Pressed(); // 램프 켜는 기능 수행
+        }
+    }
+
+    public interface ICommand
+    {
+        public abstract void Execute();
+    }
+
+    public class Button
+    {
+        private ICommand theCommand;
+        // 생성자에서 버튼을 눌렀을 때 필요한 기능을 인지로 받는다.
+        public Button(ICommand theCommand) { SetCommand(theCommand); }
+        public void SetCommand(ICommand newCommand) { this.theCommand = newCommand; }
+        // 버튼이 눌리면 주어진 Command의 execute 메서드를 호출한다.
+        public void Pressed() { theCommand.Execute(); }
+    }
+
+    public class Lamp
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Lamp On");
+        }
+    }
+
+    // 램프를 켜는 LampOnCommand 클래스
+    public class LampOnCommand : ICommand
+    {
+        private Lamp theLamp;
+
+        public LampOnCommand(Lamp theLamp) { this.theLamp = theLamp; }
+        // Command 인터페이스의 execute 메서드
+        public void Execute() { theLamp.TurnOn(); }
+    }
+
+    public class Alarm
+    {
+        public void Start() { Console.WriteLine("Alarming"); }
+    }
+
+    // 알람을 울리는 AlarmStartCommand 클래스
+    public class AlarmStartCommand : ICommand
+    {
+        private Alarm theAlarm;
+
+        public AlarmStartCommand(Alarm theAlarm) { this.theAlarm = theAlarm; }
+        // Command 인터페이스의 execute 메서드
+        public void Execute() { theAlarm.Start(); }
+    }
+}
